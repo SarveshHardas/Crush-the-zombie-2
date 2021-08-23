ebbb2ff6520aa4cd7406d7afcf82d7b53348d17e
SarveshHardas <86554291+SarveshHardas@users.noreply.github.com>
2021-08-23 07:46:51 +0530

    Add files via upload

---
+++ js/Stone.js
@@ -0,0 +1,28 @@
+class Stone
+{
+    constructor(x,y,radius)
+    {
+        var options={
+            restitution:0.8,
+            friction:0.8,
+        }
+        this.x=x;
+        this.y=y;
+        this.radius=radius;
+        this.image = loadImage("assets/stone.png");
+        this.body=Bodies.circle(x,y,radius,options);
+        World.add(world,this.body)
+    }
+    display()
+    {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        fill(255,255,255)
+        translate(pos.x,pos.y)
+        rotate(angle)
+        rectMode(CENTER)
+        image(this.image,0,0,this.radius)
+        pop();
+    }
+}
